00647bfebb3774205817b1bc2b1be2811c427337
MatheusBraga71 <matheus_pivoto@gec.inatel.br>
2023-09-12 21:46:40 -0300

    Adicionando o annotation @Before

--- Aula7/TesteMock/src/test/java/TesteBuscaInimigo.java
+++ Aula7/TesteMock/src/test/java/TesteBuscaInimigo.java
@@ -1,19 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TesteBuscaInimigo {
 
+    InimigoService service;
+    BuscaInimigo buscaInimigo;
+    @Before
+    public void setup(){
+        service = new MockInimigoService();
+        buscaInimigo = new BuscaInimigo(service);
+    }
+
     @Test
     public void testeBuscaInimigo(){
 
-         InimigoService service = new MockInimigoService();
-         BuscaInimigo buscaInimigo = new BuscaInimigo(service);
+        Inimigo bulbasaur = buscaInimigo.buscaInimigo(10);
 
-         Inimigo bulbasaur = buscaInimigo.buscaInimigo(10);
-
-         assertEquals("Bulbasaur", bulbasaur.getNome());
-         assertEquals(200.0, bulbasaur.getQtdVida(),0.1);
-         assertEquals("Solar Beam", bulbasaur.getArma());
+        assertEquals("Bulbasaur", bulbasaur.getNome());
+        assertEquals(200.0, bulbasaur.getQtdVida(),0.1);
+        assertEquals("Solar Beam", bulbasaur.getArma());
 
     }
+
+    @Test
+    public void testeBuscaInimigoInvalido(){
+
+        Inimigo inimigo_invalido = buscaInimigo.buscaInimigo(-10);
+    }
+
 }
